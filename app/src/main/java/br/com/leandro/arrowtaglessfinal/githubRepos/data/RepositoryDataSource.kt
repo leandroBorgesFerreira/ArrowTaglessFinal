73e9cb7a847491b3ff534fd2857cbb4471fcec71
Leandro Borges Ferreira <lehen01@gmail.com>
2018-11-06 08:30:54 -0200

    fixing data source

--- app/src/main/java/br/com/leandro/arrowtaglessfinal/githubRepos/data/RepositoryDataSource.kt
+++ app/src/main/java/br/com/leandro/arrowtaglessfinal/githubRepos/data/RepositoryDataSource.kt
@@ -2,19 +2,19 @@ package br.com.leandro.arrowtaglessfinal.githubRepos.data
 
 import arrow.Kind
 import arrow.effects.typeclasses.Async
+import arrow.integrations.retrofit.adapter.unwrapBody
 import br.com.leandro.arrowtaglessfinal.githubRepos.domain.Repository
 import br.com.leandro.arrowtaglessfinal.retrofit.ApiClient
-import kotlinx.coroutines.Dispatchers
 
 class RepositoryDataSource<F>(
     private val apiClient: ApiClient,
     async: Async<F>
 ) : Async<F> by async {
+
     fun fetchAllRepositories(): Kind<F, List<Repository>> =
-                invoke(Dispatchers.IO) {
-                    apiClient.getRepositories("Java", "star", 1)
-                        .execute()
-                        .body()!!
-                        .items
-                }
+        apiClient.getRepositories("Java", "star", 1)
+            .async(this)
+            .flatMap { response -> response.unwrapBody(this) }
+            .map { dto -> dto.items }
+
 }
--- app/src/main/java/br/com/leandro/arrowtaglessfinal/retrofit/ApiClient.kt
+++ app/src/main/java/br/com/leandro/arrowtaglessfinal/retrofit/ApiClient.kt
@@ -1,8 +1,7 @@
 package br.com.leandro.arrowtaglessfinal.retrofit
 
-import arrow.effects.IO
+import arrow.integrations.retrofit.adapter.CallK
 import br.com.leandro.arrowtaglessfinal.githubRepos.dto.GithubAnswerDto
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,12 +10,7 @@ interface ApiClient {
     @GET("search/repositories")
     fun getRepositories(@Query("q") language: String,
                         @Query("sort") order: String,
-                        @Query("page") page: Int): Call<GithubAnswerDto>
-
-    @GET("search/repositories")
-    fun getRepositoriesObs(@Query("q") language: String,
-                        @Query("sort") order: String,
-                        @Query("page") page: Int): IO<GithubAnswerDto>
+                        @Query("page") page: Int): CallK<GithubAnswerDto>
 }
 
 fun apiClient() : ApiClient = retrofit().create(ApiClient::class.java)
